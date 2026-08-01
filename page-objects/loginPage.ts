854ae4f0fb2b2a330967bb4812288094ff06d6a9
micrzy <y.zeng.ru@gmail.com>
2026-08-01 16:37:17 +0200

    t

--- page-objects/loginPage.ts
+++ page-objects/loginPage.ts
@@ -1,22 +1,21 @@
-import {Page} from "@playwright/test"
+import { Page } from "@playwright/test";
 
-export class LoginPage{
+export class LoginPage {
+  constructor(readonly page: Page) {}
 
-    constructor(readonly page:Page){}
+  async navigate() {
+    await this.page.goto('/')
+    await this.page.locator('[data-test="nav-sign-in"]').click()
 
-    async navigate(){
-        await this.page.goto('/')
-        await this.page.locator('[data-test="nav-sign-in"]').click()
-    }
+  }
 
-    async loginViaEmailAndPassword(email:string,password:string){
-        await this.page.locator('[data-test="email"]').fill(email)
-        await this.page.locator('[data-test="password"]').fill(password)
-        await this.page.locator('[data-test="login-submit"]').click()
-    }
+  async loginViaEmailAndPassword(email: string, password: string) {
+    await this.page.locator('[data-test="email"]').fill(email);
+    await this.page.locator('[data-test="password"]').fill(password);
+    await this.page.locator('[data-test="login-submit"]').click();
+  }
 
-    get errorAlert(){
-        return this.page.locator('[data-test="login-error"]')
-    }
-
+  get errorAlert() {
+    return this.page.locator('[data-test="login-error"]');
+  }
 }
