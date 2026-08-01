c32bc8d9b22e90b45f5dada0c54e82d4e63392df
micrzy <y.zeng.ru@gmail.com>
2026-08-01 16:18:52 +0200

    tt

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect } from "../page-objects/fixtures.ts";
 
 test.describe("Login tests", () => {
-
   test.beforeEach(async ({ poManager }) => {
-  await poManager.loginPage.navigate();
-});
+    await poManager.loginPage.navigate();
+  });
   test("Login failed", async ({ poManager }) => {
-    await poManager.loginPage.loginViaEmailAndPassword("wrong@user.com", "123456");
+    await poManager.loginPage.loginViaEmailAndPassword(
+      "wrong@user.com",
+      "123456",
+    );
     await expect(poManager.loginPage.errorAlert).toContainText(
       "Invalid email or password",
     );
@@ -16,9 +18,16 @@ test.describe("Login tests", () => {
       process.env.TEST_EMAIL!,
       process.env.TEST_PASSWORD!,
     );
+
+    console.log("Current URL:", page.url());
+   
+    console.log("Email defined?:", !!process.env.TEST_EMAIL);
+
+
     await expect(page).toHaveURL(/\/account/);
-    await page.locator('[data-test="nav-menu"]').waitFor({ state: 'visible' });
-    await expect(page.locator('[data-test="nav-menu"]')).toContainText("Jack", { timeout: 10000 }); 
-    
+    await page.locator('[data-test="nav-menu"]').waitFor({ state: "visible" });
+    await expect(page.locator('[data-test="nav-menu"]')).toContainText("Jack", {
+      timeout: 10000,
+    });
   });
 });
