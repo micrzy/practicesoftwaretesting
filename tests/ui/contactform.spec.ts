988ead27f45246930bd4389c24545a0f9fb5656e
micrzy <y.zeng.ru@gmail.com>
2026-08-27 15:10:00 +0200

    modify folder

--- tests/ui/contactform.spec.ts
+++ tests/ui/contactform.spec.ts
@@ -6,7 +6,7 @@ test.describe("contact form testing",()=>{
         await poManager.contactPage.enterNameAndEmail('Karina','Sue','test@test.com')
         await poManager.contactPage.enterMessage("This is a test message.This is a test message.This is a test message.")
         await poManager.contactPage.selectSubject()
-        await poManager.contactPage.uploadAttachment('tests/fixtures/test.txt')
+        await poManager.contactPage.uploadAttachment('tests/ui/fixtures/test.txt')
         await expect(page.locator('[formcontrolname="attachment"]')).toHaveValue(/test\.txt$/)
     })
 })
